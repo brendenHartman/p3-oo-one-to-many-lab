11596e96aff7e2968afbe0abae3fab835f8bb235
Brenden Hartman <brendenhart22@gmail.com>
2024-04-29 19:20:22 -0500

    worked with inter object relationships

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,42 @@
 class Pet:
-    pass
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+    def __init__(self, name, pet_type, owner = ""):
+        self.name = name
+        Pet.set_pet_type(pet_type)
+        self.owner = owner
+        Pet.all.append(self)
+
+    @classmethod
+
+    def get_pet_type(cls):
+        return cls.pet_type
+    
+    @classmethod
+
+    def set_pet_type(cls, pet_type):
+        if pet_type in Pet.PET_TYPES:
+            cls.pet_type = pet_type
+        else:
+            raise Exception()
+
+    pet_type = property(get_pet_type, set_pet_type)
+    
 
 class Owner:
-    pass
+
+    def __init__(self, name):
+        self.name = name
+    
+    def pets(self):
+        return Pet.all
+    
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner = self
+        else:
+            raise Exception()
+
+    def get_sorted_pets(self):
+        return sorted(Pet.all, key=lambda pet: pet.name)
